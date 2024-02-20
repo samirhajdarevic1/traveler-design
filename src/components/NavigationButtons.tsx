d243fa404f2ecb0113addf91e182a68a095b341a
samir <samirhajdarevic1995@gmail.com>
2024-02-20 08:36:33 +0100

    Small fix

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 function NavigationButtons({ submitForm }: { submitForm: () => void }) {
   return (
-    <div className="flex justify-between w-[360px] align-baseline">
+    <div className="flex justify-between w-[360px] items-baseline">
       <Link href="/error">{"<"} Back</Link>
       <button
         className="p-4 bg-black text-white rounded-lg"
